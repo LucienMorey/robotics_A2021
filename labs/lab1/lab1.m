161115a6dd7fb8ce6cd1e12d5631c2b536917dda
Lucien Morey <lucienp@live.com.au>
2021-02-22 16:19:24 +1100

    local frame transform is now an option instead of global

--- labs/lab1/lab1.m
+++ labs/lab1/lab1.m
@@ -18,27 +18,37 @@ T_car_red = se2(300,550,0);
 hold on
 h_T_car_red = trplot2(T_car_red,'frame', 'car_red', 'color', 'r', 'length', 50);
 
+red_radius = sqrt(((T_car_red(1,3)-centre_x)^2 + (T_car_red(2,3)-centre_y)^2));
 
 %create the blue car
 T_car_blue = se2(300, 125, 0);
 h_T_car_blue = trplot2(T_car_blue, 'frame', 'car_blue', 'color', 'b', 'length', 50);
+blue_radius = sqrt(((T_car_blue(1,3)-centre_x)^2 + (T_car_blue(2,3)-centre_y)^2));
 
 %create static rotations to move cars
-T_rotation_red = se2(centre_x,centre_y)* se2(0,0,-rotation_speed)*se2(-centre_x,-centre_y,0);
-T_rotation_blue = se2(centre_x,centre_y)* se2(0,0,rotation_speed)*se2(-centre_x,-centre_y,0);
+
+% Global frame: only have one of these uncommented
+%T_rotation_red = se2(centre_x,centre_y)* se2(0,0,-rotation_speed)*se2(-centre_x,-centre_y,0);
+%T_rotation_blue = se2(centre_x,centre_y)* se2(0,0,rotation_speed)*se2(-centre_x,-centre_y,0);
+
+%angle increment in degrees
+angle_of_increment = 1;
+%Local Frame
+T_rotation_red = se2(sqrt(2*red_radius^2 - 2*(red_radius^2)*cos(angle_of_increment * (pi/180))),0,0) * se2(0,0,-angle_of_increment*(pi/180));
+T_rotation_blue = se2(sqrt(2*blue_radius^2 - 2*(blue_radius^2)*cos(angle_of_increment * (pi/180))),0,0) * se2(0,0,angle_of_increment*(pi/180));
 
 % create transform text for overlay on image
 message = sprintf('%0.2e  %0.2e  %0.2e\n%0.2e  %0.2e  %0.2e\n%0.2e  %0.2e  %0.2e',T_car_red(1,1),T_car_red(1,2),T_car_red(1,3),T_car_red(2,1),T_car_red(2,2),T_car_red(2,3),T_car_red(3,1),T_car_red(3,2),T_car_red(3,3));
 tf_text= text(10, 50, message, 'FontSize', 10, 'Color', [.6 .2 .6]);
 
 i = 0;
-while i < 100
+while i < 360
     % update plot 1
     nexttile(1,[1,1]);
     
     %transform car positions
-    T_car_red = T_rotation_red*T_car_red;
-    T_car_blue = T_rotation_blue*T_car_blue;
+    T_car_red = T_car_red*T_rotation_red;
+    T_car_blue = T_car_blue*T_rotation_blue;
     
     %update graph handles for cars
     delete(h_T_car_red);
@@ -61,7 +71,7 @@ while i < 100
     distance = sqrt(T_red_to_blue(1,3)^2 + T_red_to_blue(2,3)^2);
     
     % add distance and time to end of arrays
-    x = [x, i*0.01];
+    x = [x, i];
     y = [y, distance];
     
     %plot distance vs time
