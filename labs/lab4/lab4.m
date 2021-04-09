dcc58763c408e531cfb919defe23b3dacb66e729
Lucien Morey <lucienp@live.com.au>
2021-04-09 13:24:21 +1000

    completed q2/3 for week 4

--- labs/lab4/lab4.m
+++ labs/lab4/lab4.m
@@ -50,3 +50,97 @@ for circleHalf = 1:2
         drawnow();    
     end
 end
+
+%% Question 2
+close all;
+clear;
+
+mdl_puma560;
+
+T1 = transl(0.5,-0.4,0.5);
+
+q1 = p560.ikine(T1);
+
+T2 = transl(0.5,0.4,0.1);
+
+q2 = p560.ikine(T2);
+
+steps = 50;
+
+% 1 for quentic polynomial and anything else for trapezoidal
+traj_profile = 2;
+
+if traj_profile == 1
+    qMatrix = jtraj(q1,q2,steps);
+    figure(1);
+    p560.plot(qMatrix,'trail','r-');
+    velocity = zeros(steps,6);
+    acceleration = zeros(steps,6);
+    for i = 2:steps
+        velocity(i,:) = qMatrix(i,:) - qMatrix(i-1,:);
+        acceleration(i,:) = velocity(i,:) - velocity(i-1,:);
+    end
+    for i = 1:6
+        figure(2)
+        subplot(3,2,i)
+        plot(qMatrix(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Angle (rad)')
+        refline(0,p560.qlim(i,1)) % Reference line on the lower joint limit for joint i
+        refline(0,p560.qlim(i,2)) % Reference line on the upper joint limit for joint i
+     
+        figure(3)
+        subplot(3,2,i)
+        plot(velocity(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Velocity')
+      
+        figure(4)
+        subplot(3,2,i)
+        plot(acceleration(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Acceleration')
+      end
+
+else
+    s = lspb(0,1,steps);
+    qMatrix = nan(steps,6);
+    for i = 1:steps
+        qMatrix(i,:) = (1-s(i))*q1 + s(i)*q2;
+    end
+    figure(1);
+    p560.plot(qMatrix,'trail','r-');
+    velocity = zeros(steps,6);
+    acceleration = zeros(steps,6);
+    for i = 2:steps
+        velocity(i,:) = qMatrix(i,:) - qMatrix(i-1,:);
+        acceleration(i,:) = velocity(i,:) - velocity(i-1,:);
+    end
+    for i = 1:6
+        figure(2)
+        subplot(3,2,i)
+        plot(qMatrix(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Angle (rad)')
+        refline(0,p560.qlim(i,1)) % Reference line on the lower joint limit for joint i
+        refline(0,p560.qlim(i,2)) % Reference line on the upper joint limit for joint i
+     
+        figure(3)
+        subplot(3,2,i)
+        plot(velocity(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Velocity')
+      
+        figure(4)
+        subplot(3,2,i)
+        plot(acceleration(:,i),'k','LineWidth',1)
+        title(['Joint ', num2str(i)])
+        xlabel('Step')
+        ylabel('Joint Acceleration')
+      end
+end
